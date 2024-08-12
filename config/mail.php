72e5c9bced54567fc13a290f3993148b683f2f27
kradworkz <80160702+kradworkz@users.noreply.github.com>
2024-08-12 15:30:20 +0800

    asda

--- config/mail.php
+++ config/mail.php
@@ -96,8 +96,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@eulims.com'),
-        'name' => env('MAIL_FROM_NAME', 'Eulims'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@dostrstw.com'),
+        'name' => env('MAIL_FROM_NAME', 'DOST-IX RSTW'),
     ],
 
 ];
